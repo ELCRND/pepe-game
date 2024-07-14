0f8f37b2342a507e824fdc877ef93c7d78f170a4
ELCRND <jwhh2a@gmail.com>
2024-07-14 14:06:21 +0300

    new file:   css/styles.css 	new file:   css/styles.css.map 	new file:   index.html 	new file:   js/main.js 	new file:   package.json 	new file:   public/img/about/about_img.png 	new file:   public/img/about/about_img_mobile.png 	new file:   public/img/creator/creator_img.png 	new file:   public/img/creator/creator_img_mobile.png 	new file:   public/img/home/home_img.png 	new file:   public/img/home/home_img_mobile.png 	new file:   public/img/roadmap/roadmap_img.png 	new file:   public/img/roadmap/roadmap_img_mobile.png 	new file:   scss/base/_base.scss 	new file:   scss/base/_reset.scss 	new file:   scss/blocks/about.scss 	new file:   scss/blocks/creator.scss 	new file:   scss/blocks/footer.scss 	new file:   scss/blocks/header.scss 	new file:   scss/blocks/home.scss 	new file:   scss/main.scss

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector(".header__menu-btn");
+const menu = document.querySelector(".header__menu");
+
+// открыть/закрыть меню
+menuBtn.addEventListener("click", () => {
+  menu.classList.toggle("visible");
+  menuBtn.classList.toggle("close");
+  document.body.classList.toggle("scroll-off");
+});
+
+// закрытие меню при нажатии на ссылку
+document.querySelectorAll(".menu__link").forEach((link) =>
+  link.addEventListener("click", () => {
+    menu.classList.remove("visible");
+    menuBtn.classList.remove("close");
+    document.body.classList.remove("scroll-off");
+  })
+);
